4be0fe6e4135872027b6d02fc8a78ef418a006f1
Wu Jie(exp) <KarasAya@gmail.com>
2010-11-19 14:47:34 +0800

    update document

--- src/container/array.c
+++ src/container/array.c
@@ -137,3 +137,49 @@ void* ex_array_push_back_nomng ( ex_array_t* _array, void* _value )
     ++_array->_length;
     return val_addr;
 }
+
+// ------------------------------------------------------------------ 
+// Desc: 
+// ------------------------------------------------------------------ 
+
+// managed
+void ex_array_fast_erase ( ex_array_t* _array, size_t _idx )
+{
+    void *val_addr, *last_val_addr;
+
+    ex_assert_return( _idx >= 0 && _idx < (int)_array->_length, /*dummy*/, "error: _idx out of range" );
+
+    // don't do any thing if we only have one element in the array.
+    if ( ex_array_len(_array) != 1 ) {
+        // over write the erased element address by fill it with the last element.
+        val_addr = (char*)(_array->_data) + _idx * _array->_element_bytes;
+        last_val_addr = (char*)(_array->_data) + _array->_length * _array->_element_bytes;
+        memcpy ( val_addr, last_val_addr, _array->_element_bytes );
+    }
+
+    // count the size
+    --_array->_length;
+
+    // TODO: shrink the array by exsdk's memory manager.
+}
+
+// no managed
+void ex_array_fast_erase_nomng ( ex_array_t* _array, size_t _idx )
+{
+    void *val_addr, *last_val_addr;
+
+    ex_assert_return( _idx >= 0 && _idx < (int)_array->_length, /*dummy*/, "error: _idx out of range" );
+
+    // don't do any thing if we only have one element in the array.
+    if ( ex_array_len(_array) != 1 ) {
+        // over write the erased element address by fill it with the last element.
+        val_addr = (char*)(_array->_data) + _idx * _array->_element_bytes;
+        last_val_addr = (char*)(_array->_data) + _array->_length * _array->_element_bytes;
+        memcpy ( val_addr, last_val_addr, _array->_element_bytes );
+    }
+
+    // count the size
+    --_array->_length;
+
+    // TODO: shrink the array by system memory operation.
+}
--- src/container/array.h
+++ src/container/array.h
@@ -101,56 +101,193 @@ typedef struct ex_array_t {
  ex_array_t* my_array = ex_array_alloc ( sizeof(float), 10 );
  @endcode
 
- @note you need to use ex_array_free to release the memeory, other wise it will
- have memory leak.
+ @note you need to use \ref ex_array_free to release the memeory, other wise it will
+ lead to memory leak.
 
  @sa ex_array_alloc_nomng
+
+ @fn ex_array_t* ex_array_alloc_nomng ( size_t _element_bytes, size_t _count )
+ @param _element_bytes the bytes of the element in the array
+ @param _count the default count of the element in the array
+ @details create and return an array by given size and count of the element in it.
+ the array is alloc by system memory, and you must use same memory operation 
+ command on it (those function \b with \em _nomng suffix)
+
+ @note you need to use \ref ex_array_free_nomng to release the memeory, other wise it will
+ lead to memory leak.
+
+ @sa ex_array_alloc
 */// ------------------------------------------------------------------ 
 
 ex_array_t* ex_array_alloc ( size_t _element_bytes, size_t _count );
 ex_array_t* ex_array_alloc_nomng ( size_t _element_bytes, size_t _count );
 
 // ------------------------------------------------------------------ 
-// Desc: 
-// ------------------------------------------------------------------ 
+/*! 
+ @fn void ex_array_free ( ex_array_t* _array )
+ @param _array the allocated array.
+ @details destroy the allocated array, release the memory by exsdk's memory manager.
+ @sa ex_array_free_nomng
+
+ @fn void ex_array_free_nomng ( ex_array_t* _array )
+ @param _array the allocated array.
+ @details destroy the allocated array, release the memory by system memory operation.
+ @sa ex_array_free
+*/// ------------------------------------------------------------------ 
 
 void ex_array_free ( ex_array_t* _array );
 void ex_array_free_nomng ( ex_array_t* _array );
 
 // ------------------------------------------------------------------ 
-// Desc: 
-// ------------------------------------------------------------------ 
+/*! 
+ @fn void* ex_array_get ( ex_array_t* _array, size_t _idx )
+ @param _array the in array.
+ @param _idx the element index
+ @return get the element pointer address
+ @details get the array element by index
+
+ Example:
+ @code
+ float* result = NULL;
+ // create an array to store float, the default size is 10.
+ ex_array_t* my_array = ex_array_alloc ( sizeof(float), 10 );
+ ex_array_push_back_float ( my_array, 10.0f ); // push at index 0
+ ex_array_push_back_float ( my_array, 20.0f ); // push at index 1
+ ex_array_push_back_float ( my_array, 30.0f ); // push at index 2
+ ex_array_push_back_float ( my_array, 40.0f ); // push at index 3
+ result = (float*)ex_array_get ( my_array, 1 ); // get from index 1
+ printf( "the result is %f", *result ); // the result is 20.0f
+ @endcode
+
+ @note you must cast the type of the element by yourself.
+*/// ------------------------------------------------------------------ 
 
 void* ex_array_get ( ex_array_t* _array, size_t _idx );
 
 // ------------------------------------------------------------------ 
-// Desc: 
+/*! 
+ @fn inline size_t ex_array_len ( ex_array_t* _array )
+ @param _array the in array
+ @details get the array element count.
+*/// ------------------------------------------------------------------ 
+
+inline size_t ex_array_len ( ex_array_t* _array ) { return _array->_length; } 
+
 // ------------------------------------------------------------------ 
+/*! 
+ @fn void* ex_array_push_back ( ex_array_t* _array, void* _value )
+ @param _array the in array.
+ @param _value the value add to the array
+ @return the pointer address of the pushed element in the array
+ @details push the value to the array, the array will copy the value through
+ the parameter _value by allocating it in the passing array use exsdk's
+ memory manage method. 
+
+ Example:
+ @code
+ float* result;
+ float val = 10.0f;
+ // create an array to store float, the default size is 10.
+ ex_array_t* my_array = ex_array_alloc ( sizeof(float), 10 );
+ {
+     float val = 10.0f;
+     result = (float*)ex_array_push_back ( my_array, &val );
+ }
+ printf( "the result is %f", *result ); // the result is 10.0f
+ @endcode
+
+ @sa ex_array_push_back_nomng
+
+ @fn void* ex_array_push_back_nomng ( ex_array_t* _array, void* _value )
+ @param _array the in array.
+ @param _value the value add to the array
+ @return the pointer address of the pushed element in the array
+ @details push the value to the array, the array will copy the value through
+ the parameter _value by allocating it in the passing array use system memory 
+ operation method. 
+ @sa ex_array_push_back
+*/// ------------------------------------------------------------------ 
 
 void* ex_array_push_back ( ex_array_t* _array, void* _value );
 void* ex_array_push_back_nomng ( ex_array_t* _array, void* _value );
 
-inline void* ex_array_push_back_int8 ( ex_array_t* _array, int8 _value ) { return ex_array_push_back ( _array, &_value ); }
-inline void* ex_array_push_back_int16 ( ex_array_t* _array, int16 _value ) { return ex_array_push_back ( _array, &_value ); }
-inline void* ex_array_push_back_int32 ( ex_array_t* _array, int32 _value ) { return ex_array_push_back ( _array, &_value ); }
-inline void* ex_array_push_back_int64 ( ex_array_t* _array, int64 _value ) { return ex_array_push_back ( _array, &_value ); }
+// ------------------------------------------------------------------ 
+/*! 
+ @fn inline int8* ex_array_push_back_int8 ( ex_array_t* _array, int8 _value )
+ @details push int8 to the array
 
-inline void* ex_array_push_back_uint8 ( ex_array_t* _array, uint8 _value ) { return ex_array_push_back ( _array, &_value ); }
-inline void* ex_array_push_back_uint16 ( ex_array_t* _array, uint16 _value ) { return ex_array_push_back ( _array, &_value ); }
-inline void* ex_array_push_back_uint32 ( ex_array_t* _array, uint32 _value ) { return ex_array_push_back ( _array, &_value ); }
-inline void* ex_array_push_back_uint64 ( ex_array_t* _array, uint64 _value ) { return ex_array_push_back ( _array, &_value ); }
+ @fn inline int16* ex_array_push_back_int16 ( ex_array_t* _array, int16 _value )
+ @details push int16 to the array
 
-inline void* ex_array_push_back_float ( ex_array_t* _array, float _value ) { return ex_array_push_back ( _array, &_value ); }
-inline void* ex_array_push_back_double ( ex_array_t* _array, double _value ) { return ex_array_push_back ( _array, &_value ); }
+ @fn inline int32* ex_array_push_back_int32 ( ex_array_t* _array, int32 _value )
+ @details push int32 to the array
 
-inline void* ex_array_push_back_string ( ex_array_t* _array, char* _value ) { return ex_array_push_back ( _array, &_value ); }
-inline void* ex_array_push_back_wstring ( ex_array_t* _array, wchar_t* _value ) { return ex_array_push_back ( _array, &_value ); }
+ @fn inline int64* ex_array_push_back_int64 ( ex_array_t* _array, int64 _value )
+ @details push int64 to the array
+
+ @fn inline uint8* ex_array_push_back_uint8 ( ex_array_t* _array, uint8 _value )
+ @details push uint8 to the array
+
+ @fn inline uint16* ex_array_push_back_uint16 ( ex_array_t* _array, uint16 _value )
+ @details push uint16 to the array
+
+ @fn inline uint32* ex_array_push_back_uint32 ( ex_array_t* _array, uint32 _value )
+ @details push uint32 to the array
+
+ @fn inline uint64* ex_array_push_back_uint64 ( ex_array_t* _array, uint64 _value )
+ @details push uint64 to the array
+
+ @fn inline float* ex_array_push_back_float ( ex_array_t* _array, float _value )
+ @details push float to the array
+
+ @fn inline double* ex_array_push_back_double ( ex_array_t* _array, double _value )
+ @details push double to the array
+
+ @fn inline char** ex_array_push_back_string ( ex_array_t* _array, char* _value )
+ @details push c-string to the array
+
+ @fn inline wchar_t** ex_array_push_back_wstring ( ex_array_t* _array, wchar_t* _value )
+ @details push w-string to the array
+
+*/// ------------------------------------------------------------------ 
+
+inline int8* ex_array_push_back_int8 ( ex_array_t* _array, int8 _value ) { return (int8*)ex_array_push_back ( _array, &_value ); }
+inline int16* ex_array_push_back_int16 ( ex_array_t* _array, int16 _value ) { return (int16*)ex_array_push_back ( _array, &_value ); }
+inline int32* ex_array_push_back_int32 ( ex_array_t* _array, int32 _value ) { return (int32*)ex_array_push_back ( _array, &_value ); }
+inline int64* ex_array_push_back_int64 ( ex_array_t* _array, int64 _value ) { return (int64*)ex_array_push_back ( _array, &_value ); }
+
+inline uint8* ex_array_push_back_uint8 ( ex_array_t* _array, uint8 _value ) { return (uint8*)ex_array_push_back ( _array, &_value ); }
+inline uint16* ex_array_push_back_uint16 ( ex_array_t* _array, uint16 _value ) { return (uint16*)ex_array_push_back ( _array, &_value ); }
+inline uint32* ex_array_push_back_uint32 ( ex_array_t* _array, uint32 _value ) { return (uint32*)ex_array_push_back ( _array, &_value ); }
+inline uint64* ex_array_push_back_uint64 ( ex_array_t* _array, uint64 _value ) { return (uint64*)ex_array_push_back ( _array, &_value ); }
+
+inline float* ex_array_push_back_float ( ex_array_t* _array, float _value ) { return (float*)ex_array_push_back ( _array, &_value ); }
+inline double* ex_array_push_back_double ( ex_array_t* _array, double _value ) { return (double*)ex_array_push_back ( _array, &_value ); }
+
+inline char** ex_array_push_back_string ( ex_array_t* _array, char* _value ) { return (char**)ex_array_push_back ( _array, &_value ); }
+inline wchar_t** ex_array_push_back_wstring ( ex_array_t* _array, wchar_t* _value ) { return (wchar_t**)ex_array_push_back ( _array, &_value ); }
 
 // ------------------------------------------------------------------ 
 // Desc: 
 // ------------------------------------------------------------------ 
 
-inline size_t ex_array_len ( ex_array_t* _array ) { return _array->_length; } 
+void ex_array_erase ( ex_array_t* _array, size_t _idx );
+void ex_array_erase_nomng ( ex_array_t* _array, size_t _idx );
+
+// ------------------------------------------------------------------ 
+/*! 
+ @fn void ex_array_fast_erase ( ex_array_t* _array, size_t _idx )
+ @param _array the in array
+ @param _idx the delete element index. 
+ @details delete the element by index. the method will move the last
+ element to the delete element adress, which make it faster than \ref
+ ex_array_erase.
+ @note using the method will break the indexing order in the array.
+ @sa ex_array_fast_erase_nomng
+*/// ------------------------------------------------------------------ 
+
+void ex_array_fast_erase ( ex_array_t* _array, size_t _idx );
+void ex_array_fast_erase_nomng ( ex_array_t* _array, size_t _idx );
 
 //! @}
 
